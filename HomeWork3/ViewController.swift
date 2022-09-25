d0e2ad5c6140ed7ce2b2b515fb6d452043499be5
Albina <albina.exe@mail.ru>
2022-09-25 18:30:34 +0200

    Finished

--- HomeWork3/ViewController.swift
+++ HomeWork3/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet var screenWithColor: UIView!
+    @IBOutlet var redOpacityLevel: UILabel!
+    @IBOutlet var greenOpacityLevel: UILabel!
+    @IBOutlet var blueOpacityLevel: UILabel!
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        screenWithColor.layer.cornerRadius = 10
+        setupOpacityLevel()
     }
-
-
+    
+    //MARK: - IBActions
+    @IBAction func redSliderAction() {
+        let roundingTheRedValue = round(redColorSlider.value * 100) / 100
+        redOpacityLevel.text = roundingTheRedValue.formatted()
+        screenWithColor.backgroundColor = .red
+        screenWithColor.alpha = CGFloat(redColorSlider.value)
+        blendColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        let roundingTheGreenValue = round(greenColorSlider.value * 100) / 100
+        greenOpacityLevel.text = roundingTheGreenValue.formatted()
+        screenWithColor.backgroundColor = .green
+        screenWithColor.alpha = CGFloat(greenColorSlider.value)
+        blendColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        let roundingTheBlueValue = round(blueColorSlider.value * 100) / 100
+        blueOpacityLevel.text = roundingTheBlueValue.formatted()
+        screenWithColor.backgroundColor = .blue
+        screenWithColor.alpha = CGFloat(blueColorSlider.value)
+        blendColor()
+    }
+    
+    //MARK: - Private func
+    private func blendColor() {
+        screenWithColor.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setupOpacityLevel() {
+        redOpacityLevel.text = redColorSlider.value.formatted()
+        greenOpacityLevel.text = greenColorSlider.value.formatted()
+        blueOpacityLevel.text = blueColorSlider.value.formatted()
+    }
+    
 }
-
